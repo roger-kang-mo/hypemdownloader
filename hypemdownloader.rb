b5a4adfdc1c7b2743a8e02795550643d41869dd5
Steven Magelowitz <steven.magelowitz@gmail.com>
2015-01-06 15:46:34 -0800

    Returned pages from get_pages and also reformatted

--- hypemdownloader.rb
+++ hypemdownloader.rb
@@ -14,59 +14,60 @@ dest_url = "http://hypem.com/#{USERNAME}?ax=1"
 track_list = []
 
 def get_songs_for_page(url, mechanize)
-	track_list = []
+  track_list = []
 
-	mechanize.get(url) do |page|
-	  tracks = JSON.parse(page.search("#displayList-data").first)['tracks']
+  mechanize.get(url) do |page|
+    tracks = JSON.parse(page.search("#displayList-data").first)['tracks']
 
-	  track_list = tracks.map do |track|
-	  	url = "#{SOURCE_URL}#{track['id']}/#{track['key']}"
+    track_list = tracks.map do |track|
+      url = "#{SOURCE_URL}#{track['id']}/#{track['key']}"
 
-	  	track_data = {
-	  		artist: track['artist'],
-	  		title: track['song'],
-	  		post_url: track['posturl']
-	  	}
+      track_data = {
+          artist: track['artist'],
+          title: track['song'],
+          post_url: track['posturl']
+      }
 
-	  	begin
-				mechanize.get(url) do |page|
-					track_url = JSON.parse(page.body)['url']
+      begin
+        mechanize.get(url) do |page|
+          track_url = JSON.parse(page.body)['url']
 
-					track_data.merge({ url: track_url })
+          track_data.merge({url: track_url})
 
-					open(track_url) do |f|
-						p "Downloading #{track_data[:artist]} - #{track_data[:title]}.mp3"
+          open(track_url) do |f|
+            p "Downloading #{track_data[:artist]} - #{track_data[:title]}.mp3"
 
-					  File.write("#{track_data[:artist]} - #{track_data[:title]}.mp3", open(track_url).read, {mode: 'wb'})
-					end
-				end
-			rescue Exception => e
-				p "Couldn't download file #{track_data[:artist]} - #{track_data[:title]}."
-			end
-	  end
+            File.write("#{track_data[:artist]} - #{track_data[:title]}.mp3", open(track_url).read, {mode: 'wb'})
+          end
+        end
+      rescue Exception => e
+        p "Couldn't download file #{track_data[:artist]} - #{track_data[:title]}."
+      end
+    end
 
-	  track_list
-	end
+    track_list
+  end
 
-	track_list
+  track_list
 end
 
 def get_pages(mechanize)
-	pages = []
-	current_page = 2
+  pages = []
+  current_page = 2
 
-	mechanize.get("http://hypem.com/#{USERNAME}?ax=1") do |page|
-		page_objects = page.search(".paginator").children.to_a
+  mechanize.get("http://hypem.com/#{USERNAME}?ax=1") do |page|
+    page_objects = page.search(".paginator").children.to_a
 
-		page_objects.select! do |p|
-			p.name == 'a' && !p.children.first.text.include?("See more")
-		end
+    page_objects.select! do |p|
+      p.name == 'a' && !p.children.first.text.include?("See more")
+    end
 
-		page_objects.count.times do |t|
-			pages << "/#{current_page}"
-			current_page += 1
-		end
-	end
+    page_objects.count.times do |t|
+      pages << "/#{current_page}"
+      current_page += 1
+    end
+  end
+  pages
 end
 
 pages.concat get_pages(mechanize)
